56962518e9780ee9c2cde17c3e8e5b18116ba36f
Ostap Onyshkiv <prostoostap58@gmail.com>
2022-03-23 20:26:18 +0200

    added files

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_8.1.3/Lab_8.1.3.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char str[]=",--gfhdfgh-,m,--,---,";
+			Assert::AreEqual(Count(str, 0), 3);
+		}
+	};
+}
